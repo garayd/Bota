704519c666abea94320a2266648d5c9284cfb939
garayd <dg488@cornell.edu>
2015-04-12 16:02:18 -0400

    data proposal script

---
+++ scripts/label_freq.py
@@ -0,0 +1,23 @@
+from itertools import islice
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+_DATA = "../data/cellartracker.txt"
+
+
+def rating_dist():
+    acc = defaultdict(int)
+    with open(_DATA) as f:
+        for line in islice(f, 4, None, 10):
+            try:
+                r = int(line[15:])
+            except:
+                r = -1
+            acc[r] += 1
+    return acc
+
+
+def plot_rating_dict(d):
+    plt.bar(d.keys(), d.values())
+    plt.show()
