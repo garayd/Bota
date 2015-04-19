2193a6739f1e230add8d4d158647f0c486ab53bd
Michael <MichaelStern93@Gmail.com>
2015-04-19 13:14:19 -0400

    Started to work on pickle data loading

---
+++ ml_src/load_data.py
@@ -0,0 +1,10 @@
+import pickle
+# from itertools import islice
+# from collections import defaultdict
+
+# from nltk.tokenize import RegexpTokenizer
+# from nltk.corpus import stopwords
+
+
+DATA = "../data/cellartracker_100.txt"
+
