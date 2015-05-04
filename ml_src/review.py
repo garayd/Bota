578c814981b9d87c7fda5b987f2713f641082b2b
Michael <MichaelStern93@Gmail.com>
2015-05-04 00:44:42 -0400

    web app basically done, need to clean up a few things and take out hard coded examples

---
+++ ml_src/review.py
@@ -0,0 +1,8 @@
+class Review:
+    def __init__(self, wineId, points, time, userId, text):
+        self.wineId = wineId
+        self.points = points
+        self.time = time
+        self.text = text
+        self.userId = userId
+
---
+++ ml_src/user.py
@@ -0,0 +1,4 @@
+class User:
+    def __init__(self, userId, userName):
+        self.userId = userId
+        self.userName = userName
